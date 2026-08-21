4800424a3a71173dff3c382c6aa9131023138a0b
jasb-to <jaspalbilkhu@gmail.com>
2026-08-22 00:46:29 +0100

    feat: add The Getaway route

---
+++ app/games/the-getaway/page.tsx
@@ -0,0 +1,2 @@
+import TheGetaway from "../../../components/the-getaway";
+export default function TheGetawayPage(){return <main className="min-h-screen bg-[#090909] p-4 text-[#f5f2ea] md:p-8"><div className="mx-auto max-w-6xl"><TheGetaway/></div></main>}
