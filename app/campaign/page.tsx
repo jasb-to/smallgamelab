53898d4a488ed35882fa979e2f50dbdc0095a388
jasb-to <jaspalbilkhu@gmail.com>
2026-08-22 12:57:40 +0100

    feat: add dedicated Long Run campaign screen

---
+++ app/campaign/page.tsx
@@ -0,0 +1,2 @@
+import LongRunHub from "../../components/long-run-hub";
+export default function CampaignPage(){return <main className="min-h-screen px-6 lg:px-10"><div className="mx-auto max-w-7xl"><LongRunHub/></div></main>}
