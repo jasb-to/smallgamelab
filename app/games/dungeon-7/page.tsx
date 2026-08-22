4a5da09e5e17a0f13c6b9596286975fd4116b05f
jasb-to <jaspalbilkhu@gmail.com>
2026-08-22 01:13:24 +0100

    feat: add Dungeon 7 route

---
+++ app/games/dungeon-7/page.tsx
@@ -0,0 +1,2 @@
+import DungeonSeven from "../../../components/dungeon-seven";
+export default function DungeonSevenPage(){return <main className="min-h-screen bg-[#090909] p-4 text-[#f5f2ea] md:p-8"><div className="mx-auto max-w-6xl"><DungeonSeven/></div></main>}
