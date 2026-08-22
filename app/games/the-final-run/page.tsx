6ded5c2092cd737e1f9585c569b7adbfdd9edf78
jasb-to <jaspalbilkhu@gmail.com>
2026-08-22 10:10:27 +0100

    feat: add The Final Run route

---
+++ app/games/the-final-run/page.tsx
@@ -0,0 +1,2 @@
+import TheFinalRun from "../../../components/the-final-run";
+export default function TheFinalRunPage(){return <main className="min-h-screen bg-[#090909] p-4 text-[#f5f2ea] md:p-8"><div className="mx-auto max-w-6xl"><TheFinalRun/></div></main>}
